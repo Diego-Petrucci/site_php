9886e7eccb99009cff69e1ee79ce8a4bab9dc982
Diego Petrucci <digopetrucci29@gmail.com>
2022-11-30 01:20:18 -0300

    ações

---
+++ editar.php
@@ -0,0 +1,147 @@
+<?php
+    session_start();
+    if(!empty($_GET['id'])){
+        
+        include_once('config.php');
+
+        $id = $_GET['id'];
+
+        $sqlSelect = "SELECT  * FROM playlists WHERE id = $id"; 
+
+        $result = $conexao -> query($sqlSelect);
+
+        if($result -> num_rows > 0 ){
+            while($user_data = mysqli_fetch_assoc($result)){
+                $musica = $user_data['musica'];
+                $artista = $user_data['artista'];
+            }
+        }else{
+            header('Location: playlist.php');
+        }
+
+            
+
+       $result = mysqli_query($conexao, "INSERT INTO playlists (artista, musica, id_user)
+       VALUES ('$artista', '$musica', '$id_user')");
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Editar músicas</title>
+    <style>
+        body{
+            font-family: Arial, Helvetica, sans-serif;
+            background-image: linear-gradient(135deg, #001247 0%, #00bfff 100%);
+        }
+        .box{
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform:translate(-50%, -50%);
+            background-color: rgba(0, 0, 0, 0.6);
+            padding: 15px;
+            border-radius: 20px;
+            width: 20%;
+            color: white;
+        }
+        fieldset{
+            border: 3px solid;
+            border-radius: 15px;
+            border-color: #00486f ;
+        }
+        legend{
+            padding: 10px;
+            text-align: center;
+            background-color: #00486f ;
+            border-radius: 10px;
+        }
+        .inputBox{
+            position: relative;
+        }
+        .inputUser{
+            background: none;
+            border: none;
+            border-bottom: 1px solid white;
+            outline: none;
+            color: white;
+            font-size: 15px;
+            width: 100%;
+            letter-spacing: 2px;
+        }
+        .labelInput{
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            pointer-events: none;
+            transition: .5s;
+        }
+        .inputUser:focus ~ .labelInput,
+        .inputUser:valid ~ .labelInput{
+            top: -20px;
+            font-size: 12px;
+            color: #00486f  ;
+        }
+        #data_nascimento{
+            border: none;
+            padding-left: 20px;
+            padding-right: 20px;
+            padding-top: 8px;
+            padding-bottom: 8px;
+            border-radius: 10px;
+            outline: none;
+            font-size: 15px;
+        }
+        #submit{
+            background-color:  #00486f ;
+            border: none;
+            width: 100%;
+            padding: 15px;
+            color: white;
+            font-size: 20px;
+            border-radius: 20px;
+            cursor: pointer;
+        }
+        #submit:hover{
+            background-color:deepskyblue;
+        }
+        a{
+            position: absolute;
+            text-decoration: none;
+            color: white;
+            border: 3px solid dodgerblue;  
+            border-radius: 15px;
+            padding: 20px;
+            background-color: dodgerblue;
+        }
+        a:hover{
+            background-color: red;
+            border: 3px solid red; 
+        }
+    </style>
+</head>
+<body>
+    <a href="playlist.php">VOLTAR</a>
+    <div class="box">
+        <form action="editar.php" method="POST" enctype="multipart/form-data">
+            <fieldset>
+                <legend><b>Edite as informações</b></legend>
+                <br>
+                <div class="inputBox">
+                    <input type="text" name="musica" id="musica" class="inputUser" value="<?php echo $musica ?>">
+                    <label for="musica" class="labelInput">Nome da Música</label>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="text" name="artista" id="artista" class="inputUser" value="<?php echo $artista ?>">
+                    <label for="artista" class="labelInput">Artista</label>
+                <input type="submit" name="submit" id="submit" value="Salvar alterações">
+            </fieldset>
+        </form>
+    </div>
+</body>
+</html>
--- playlist.php
+++ playlist.php
@@ -1,11 +1,15 @@
 <?php
     session_start();
-    print_r($_SESSION);
+    include_once('config.php');
+    //print_r($_SESSION);
     if((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true)){
         unset($_SESSION['email']);
         unset($_SESSION['senha']);
         header('Location: home.php');
     }
+    $sql = "SELECT * FROM playlists WHERE id_user = {$_SESSION['id_user']}";
+    $result =$conexao -> query($sql);    
+    //print_r($result);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,10 +17,49 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Playlist</title>
+    <style>
+        body{
+            color: black;
+            text-align: center;
+        }
+        table, th, td{
+            position: relative;
+            border: 1px solid black;
+            border-collapse: collapse;
+            padding-top: 30px;
+            padding-left: 80px;
+            padding-right: 80px;
+            padding-bottom: 30px;
+        }
+        table{
+            width: 100%;
+        }
+    </style>
 </head>
 <body>
     <a href="adicionar.php">ADICIONAR</a>
     <h1>PLAYLISTS
+    <div class='tabela'>
+        <table>
+            <tr>
+                <th>id</th>
+                <th>Música</th>
+                <th>Artista</th>
+                <th>Ações</th>
+            </tr>
+            <tbody>
+                <?php
+                    while($user_data = mysqli_fetch_assoc($result)){
+                        echo "<tr>";
+                        echo "<td>".$user_data['id']."</td>";
+                        echo "<td>".$user_data['musica']."</td>";
+                        echo "<td>".$user_data['artista']."</td>";
+                        echo "<td> <a href='editar.php'>EDITAR</a> <a href='remover.php'>REMOVER</a> </td>";
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
